0b5b32c2e7627a40b3aaf110dacdc5cff1d8d204
Marek Mikula <marek.mikula01@gmail.com>
2020-08-18 20:32:36 +0200

    Initial test

---
+++ tests/Feature/InitialTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace MarekMikula\LaravelConsoleEvents\Tests;
+
+use Orchestra\Testbench\TestCase;
+
+class InitialTest extends TestCase
+{
+	/** @test */
+	public function initial_test()
+	{
+		$this->assertTrue(true);
+	}
+}
